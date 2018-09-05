cc0c736432cc0a7ea16f261a5f224215625d649b
Krzysztof Kowalski <kowalski.cv@gmail.com>
2018-09-05 20:05:32 +0200

    no lint after all...

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import 'reset-css';
-import 'normalize-css';
+import 'normalize.css';
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
