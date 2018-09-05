d95ed8f090f7062653fa7116e8f741c25d4cf39b
Krzysztof Kowalski <kowalski.cv@gmail.com>
2018-09-05 21:15:46 +0200

    further setup, added enzyme, app folders etc

---
+++ src/components/Login/components/LoginPage.js
@@ -0,0 +1,17 @@
+import React, {PureComponent} from 'react';
+import LoginPageWrapper from '../styled/LoginPageWrapper';
+import LoginFormWrapper from '../styled/LoginFormWrapper';
+
+class Login extends PureComponent {
+    render(){
+        return (
+            <LoginPageWrapper>
+                <LoginFormWrapper>
+                    <h1>lol</h1>
+                </LoginFormWrapper>
+            </LoginPageWrapper>
+        );
+    }
+}
+
+export default Login;
---
+++ src/components/Login/styled/LoginPageWrapper.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const LoginPageWrapper = styled.div`
+    display: flex;
+    height: 1000px;
+    width: 100%;
+    background-color: tomato;
+    justify-content: center;
+    align-itmes: center;
+    position: relative;
+`;
+
+export default LoginPageWrapper;
