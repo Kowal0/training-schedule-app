f191315885fb12715693a58c8ba433cf84da5646
Malga90 <savemeebarry@gmail.com>
2018-09-08 22:00:54 +0200

    changing background image

--- src/components/Login/styled/LoginPageWrapper.js
+++ src/components/Login/styled/LoginPageWrapper.js
@@ -1,13 +1,15 @@
 import styled from 'styled-components';
+import myImage from './img/img1.png';
 
 export const LoginPageWrapper = styled.div`
     display: flex;
-    height: 1000px;
+    height: 100vh;
     width: 100%;
     background-color: tomato;
     justify-content: center;
     align-itmes: center;
     position: relative;
+    background-image: url(${myImage});
 `;
 
 export default LoginPageWrapper;
