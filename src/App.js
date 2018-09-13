d704fd586fb8928eebcb8f9f44c1c11b52cb3956
Krzysztof Kowalski <kowalski.cv@gmail.com>
2018-09-13 22:20:04 +0200

    set up routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch,
+  Redirect
+} from 'react-router-dom'
 import {LoginPage} from './components/Login/components/LoginPage';
+import {ProfilePage} from './components/Profile/components/ProfilePage';
+import {RotaPage} from './components/Rota/components/RotaPage';
 import PageNavbar from './components/Login/components/PageNavbar';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <PageNavbar/>
-        <LoginPage />
-      </div>
+      <Router>
+        <Fragment>
+          <PageNavbar/>
+          <Route path="/" exact={true} component={LoginPage} />
+          <Route path="/profile" exact={true} component={ProfilePage} />
+          <Route path="/rota" exact={true} component={RotaPage} />
+        </Fragment>
+      </Router>
       
     );
   }
---
+++ src/components/Profile/styled/ProfilePage.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const ProfilePageStyle = styled.div`
+   
+    display:flex;
+    justify-content: center;
+    h1 {font-size: 70px;}
+    
+    
+`;
+
+export default ProfilePageStyle;
