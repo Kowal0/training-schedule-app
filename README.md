e4d8058084836b45f45a9d77663c62efa6329370
Malga90 <savemeebarry@gmail.com>
2018-09-16 23:22:04 +0200

    Updates

--- README.md
+++
@@ -1,1 +0,0 @@
-# training-schedule-app-frontend
--- src/components/Login/styled/PageNavbarDot.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-
-export const PageNavbarDot = styled.span`
-    color: #ba4d09;
-    font-size: 30px;
-`;
-
-export default PageNavbarDot;
